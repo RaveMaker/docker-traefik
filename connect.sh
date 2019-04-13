928888658f3935a4d91d7ad5f73361b23d531c8b
RaveMaker <RaveMaker@gmail.com>
2019-04-13 19:25:15 +0300

    fixed bash format

--- connect.sh
+++ connect.sh
@@ -5,11 +5,11 @@
 docker=/usr/bin/docker
 proxyContainerName=traefik_reverse-proxy_1
 
-$docker network ls --filter NAME=frontend --format "{{.Name}}" | while read networkName
+${docker} network ls --filter NAME=frontend --format "{{.Name}}" | while read networkName
 do
     echo "Connecting $proxyContainerName to $networkName"
-    $docker network connect $networkName $proxyContainerName
-    if [ $? -eq 0 ] ; then
-	echo "Connected"
+    ${docker} network connect ${networkName} ${proxyContainerName}
+    if [[ $? -eq 0 ]] ; then
+	    echo "Connected"
     fi
 done
--- disconnect.sh
+++ disconnect.sh
@@ -5,11 +5,11 @@
 docker=/usr/bin/docker
 proxyContainerName=traefik_reverse-proxy_1
 
-$docker network ls --filter NAME=frontend --format "{{.Name}}" | while read networkName
+${docker} network ls --filter NAME=frontend --format "{{.Name}}" | while read networkName
 do
     echo "Connecting $proxyContainerName to $networkName"
-    $docker network disconnect $networkName $proxyContainerName
-    if [ $? -eq 0 ] ; then
+    ${docker} network disconnect ${networkName} ${proxyContainerName}
+    if [[ $? -eq 0 ]] ; then
         echo "disconnected"
     fi
 done
